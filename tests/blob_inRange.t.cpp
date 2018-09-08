419b62237e631b203f66c05433dd262ca47b55e1
markdflitter <markdflitter@googlemail.com>
2018-09-08 14:53:14 +0100

    more test tidying

--- tests/blob_inRange.t.cpp
+++ tests/blob_inRange.t.cpp
@@ -3,44 +3,56 @@
 
 TEST (Blob, blob_inRange_dx_false)
 {
-	Blob b1 ("", [](double) {return 0;}, 0, 0);
-	Blob b2 ("", [](double) {return 0;}, 10, 0);
+	Blob b1 ("", [](double) {return 0.0;}, 0.0, 0.0);
+	Blob b2 ("", [](double) {return 0.0;}, 10.0, 0.0);
 
-	ASSERT_FALSE (b1.isInRange (b2, 9));
-	ASSERT_FALSE (b2.isInRange (b1, 9));
+	ASSERT_FALSE (b1.isInRange (b2, 9.0));
+	ASSERT_FALSE (b2.isInRange (b1, 9.0));
 }
 
 TEST (Blob, blob_inRange_dx_true)
 {
-	Blob b1 ("", [](double) {return 0;}, 0, 0);
-	Blob b2 ("", [](double) {return 0;}, 10, 0);
+	Blob b1 ("", [](double) {return 0.0;}, 0.0, 0.0);
+	Blob b2 ("", [](double) {return 0.0;}, 10.0, 0.0);
 
-	ASSERT_TRUE (b1.isInRange (b2, 10));
-	ASSERT_TRUE (b2.isInRange (b1, 10));
+	ASSERT_TRUE (b1.isInRange (b2, 10.0));
+	ASSERT_TRUE (b2.isInRange (b1, 10.0));
 }
 
-TEST (Blob, inRangey)
+TEST (Blob, blob_inRange_dy_false)
 {
-	Blob b1 {"mark", [](double) {return 0;}, 0, 0, 0, 0, 0, 0};
-	Blob b2 {"annette", [](double) {return 0;}, 0, 10, 0, 0, 0, 0};
+	Blob b1 ("", [](double) {return 0.0;}, 0.0, 0.0);
+	Blob b2 ("", [](double) {return 0.0;}, 0.0, 10.0);
 
-	ASSERT_FALSE (b1.isInRange (b2, 9));
-	ASSERT_FALSE (b2.isInRange (b1, 9));
-
-	ASSERT_TRUE (b1.isInRange (b2, 10));
-	ASSERT_TRUE (b2.isInRange (b1, 10));
+	ASSERT_FALSE (b1.isInRange (b2, 9.0));
+	ASSERT_FALSE (b2.isInRange (b1, 9.0));
 }
 
-TEST (Blob, inRangexy)
+TEST (Blob, blob_inRange_dy_true)
 {
-	Blob b1 {"mark", [](double) {return 0;}, 3, 4, 0, 0, 0, 0};
-	Blob b2 {"annette", [](double) {return 0;}, 6, 8, 0, 0, 0, 0};
+	Blob b1 ("", [](double) {return 0.0;}, 0.0, 0.0);
+	Blob b2 ("", [](double) {return 0.0;}, 0.0, 10.0);
 
-	ASSERT_FALSE (b1.isInRange (b2, 4));
-	ASSERT_FALSE (b2.isInRange (b1, 4));
+	ASSERT_TRUE (b1.isInRange (b2, 10.0));
+	ASSERT_TRUE (b2.isInRange (b1, 10.0));
+}
 
-	ASSERT_TRUE (b1.isInRange (b2, 5));
-	ASSERT_TRUE (b2.isInRange (b1, 5));
+TEST (Blob, blob_inRange_dxy_false)
+{
+	Blob b1 {"", [](double) {return 0.0;}, 3.0, 4.0};
+	Blob b2 {"", [](double) {return 0.0;}, 6.0, 8.0};
+
+	ASSERT_FALSE (b1.isInRange (b2, 4.0));
+	ASSERT_FALSE (b2.isInRange (b1, 4.0));
+}
+
+TEST (Blob, blob_inRange_dxy_true)
+{
+	Blob b1 {"", [](double) {return 0.0;}, 3.0, 4.0};
+	Blob b2 {"", [](double) {return 0.0;}, 6.0, 8.0};
+
+	ASSERT_TRUE (b1.isInRange (b2, 5.0));
+	ASSERT_TRUE (b2.isInRange (b1, 5.0));
 }
 
 int main (int argc, char** argv) 
