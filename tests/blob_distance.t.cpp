d89c8c904b2ad42d5224a74b48cd1b21f447e7d4
markdflitter <markdflitter@googlemail.com>
2018-09-08 13:58:20 +0100

    tidy blob_distance.t.cpp

--- tests/blob_distance.t.cpp
+++ tests/blob_distance.t.cpp
@@ -1,37 +1,55 @@
 #include <gtest/gtest.h>
 #include <blob.h>
 
-TEST (Blob, distance0)
+TEST (Blob, distance_equals_0)
 {
-	Blob b1 {"mark", [](double) {return 0;}, 10, 10, 0, 0, 0, 0};
-	Blob b2 {"annette", [](double) {return 0;}, 10, 10, 0, 0, 0, 0};
+	Blob b1 ("", [](double) {return 0;}, 10, 10);
+	Blob b2 ("", [](double) {return 0;}, 10, 10);
 
 	ASSERT_EQ (b1.distance (b2), 0);
 	ASSERT_EQ (b2.distance (b1), 0);
 }
 
-TEST (Blob, distance345)
+TEST (Blob, distance_345_triangle_at_origin)
 {
-	Blob b1 {"mark", [](double) {return 0;}, 0, 0, 0, 0, 0, 0};
-	Blob b2 {"annette", [](double) {return 0;}, 3, 4, 0, 0, 0, 0};
+	Blob b1 ("", [](double) {return 0;}, 0, 0);
+	Blob b2 ("", [](double) {return 0;}, 3, 4);
 
 	ASSERT_EQ (b1.distance (b2), 5);
 	ASSERT_EQ (b2.distance (b1), 5);
 }
 
-TEST (Blob, distancen345)
+TEST (Blob, distance_minus_345_triangle_at_origin)
 {
-	Blob b1 {"mark", [](double) {return 0;}, 0, 0, 0, 0, 0, 0};
-	Blob b2 {"annette", [](double) {return 0;}, -3, -4, 0, 0, 0, 0};
+	Blob b1 ("", [](double) {return 0;}, 0, 0);
+	Blob b2 ("", [](double) {return 0;}, -3, -4);
 
 	ASSERT_EQ (b1.distance (b2), 5);
 	ASSERT_EQ (b2.distance (b1), 5);
 }
 
-TEST (Blob, distance345_2)
+TEST (Blob, distance_345_triangle_off_origin)
 {
-	Blob b1 {"mark", [](double) {return 0;}, 3, 4, 0, 0, 0, 0};
-	Blob b2 {"annette", [](double) {return 0;}, 6, 8, 0, 0, 0, 0};
+	Blob b1 ("", [](double) {return 0;}, 3, 4);
+	Blob b2 ("", [](double) {return 0;}, 6, 8);
+
+	ASSERT_EQ (b1.distance (b2), 5);
+	ASSERT_EQ (b2.distance (b1), 5);
+}
+
+TEST (Blob, distance_minus_345_triangle_off_origin)
+{
+	Blob b1 ("", [](double) {return 0;}, -3, -4);
+	Blob b2 ("", [](double) {return 0;}, -6, -8);
+
+	ASSERT_EQ (b1.distance (b2), 5);
+	ASSERT_EQ (b2.distance (b1), 5);
+}
+
+TEST (Blob, distance_minus_345_triangle_cross_origin)
+{
+	Blob b1 ("", [](double) {return 0;}, -2, -2);
+	Blob b2 ("", [](double) {return 0;}, 1, 2);
 
 	ASSERT_EQ (b1.distance (b2), 5);
 	ASSERT_EQ (b2.distance (b1), 5);
