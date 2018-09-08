169c45c908346df30711989af0f7e073e37dc7a4
markdflitter <markdflitter@googlemail.com>
2018-09-08 14:05:33 +0100

    tidy blob_history.t.cpp

--- tests/blob_history.t.cpp
+++ tests/blob_history.t.cpp
@@ -3,38 +3,38 @@
 
 TEST (Blob, blob_history)
 {
-	Blob b1 ("", [](double) {return 0;}, 10, 20);
-	b1.move (5, M_PI, "");
-	b1.move (5, M_PI, "");
-	b1.move (5, M_PI / 2, "");
+	Blob b1 ("", [](double) {return 0;}, 10.0, 20.0);
+	b1.move (5.0, M_PI, "");
+	b1.move (5.0, M_PI, "");
+	b1.move (5.0, M_PI / 2, "");
 
 	std::vector<Pt<double>> history = b1.history ();
 	ASSERT_TRUE (history.size () == 4);
-	ASSERT_TRUE (history[0].x () == 10);
- 	ASSERT_TRUE (history[0].y () == 20);
-	ASSERT_TRUE (history[1].x () == 10);
- 	ASSERT_TRUE (history[1].y () == 15);
-	ASSERT_TRUE (history[2].x () == 10);
- 	ASSERT_TRUE (history[2].y () == 10);
-	ASSERT_TRUE (history[3].x () == 15);
- 	ASSERT_TRUE (history[3].y () == 10);
+	ASSERT_TRUE (history[0].x () == 10.0);
+ 	ASSERT_TRUE (history[0].y () == 20.0);
+	ASSERT_TRUE (history[1].x () == 10.0);
+ 	ASSERT_TRUE (history[1].y () == 15.0);
+	ASSERT_TRUE (history[2].x () == 10.0);
+ 	ASSERT_TRUE (history[2].y () == 10.0);
+	ASSERT_TRUE (history[3].x () == 15.0);
+ 	ASSERT_TRUE (history[3].y () == 10.0);
 }
 
 TEST (Blob, blob_history_length)
 {
-	Blob b1 ("", [](double) {return 0;}, 10, 20);
+	Blob b1 ("", [](double) {return 0;}, 10.0, 20.0);
 	for (int i = 0; i < 1000; i++)
 	{
-		b1.move (5, 0, "");
+		b1.move (5.0, 0, "");
 	}
 	
 	std::vector<Pt<double>> history = b1.history ();
 	ASSERT_TRUE (history.size () == 500);
 	
-	ASSERT_TRUE (history[0].x () == 10);
+	ASSERT_TRUE (history[0].x () == 10.0);
  	ASSERT_TRUE (history[0].y () == 2525);
 	
-	ASSERT_TRUE (history[499].x () == 10);
+	ASSERT_TRUE (history[499].x () == 10.0);
  	ASSERT_TRUE (history[499].y () == 5020);
 }
 
