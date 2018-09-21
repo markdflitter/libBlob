9b50526157a975f1837187c3f1131876355e9b24
markdflitter <markdflitter@googlemail.com>
2018-09-21 23:31:30 +0100

    blob fight tests

--- tests/07_02_blob_fight.t.cpp
+++ tests/07_02_blob_fight.t.cpp
@@ -1,6 +1,38 @@
 #include <gtest/gtest.h>
 #include <blob.h>
 
+TEST (test_07_02_blob_fight_t, takesDamage)
+{
+	Blob b = CreateBlob ().HP (100U).lifespan (10U);
+	EXPECT_EQ (b.HP (), 100U);
+	EXPECT_FALSE (b.isDead ());
+
+	b.takeDamage (1U);
+	EXPECT_EQ (b.HP (), 99U);
+	EXPECT_FALSE (b.isDead ());
+}
+
+TEST (test_07_02_blob_fight_t, kill)
+{
+	Blob b = CreateBlob ().HP (100U).lifespan (10U);
+	EXPECT_EQ (b.HP (), 100U);
+	EXPECT_FALSE (b.isDead ());
+
+	b.takeDamage (100U);
+	EXPECT_EQ (b.HP (), 0U);
+	EXPECT_TRUE (b.isDead ());
+}
+
+TEST (test_07_02_blob_fight_t, overkill)
+{
+	Blob b = CreateBlob ().HP (100U).lifespan (10U);
+	EXPECT_EQ (b.HP (), 100U);
+	EXPECT_FALSE (b.isDead ());
+
+	b.takeDamage (1000U);
+	EXPECT_EQ (b.HP (), 0U);
+	EXPECT_TRUE (b.isDead ());
+}
 
 int main (int argc, char** argv) 
 {
