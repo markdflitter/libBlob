10048bfdbf6e81d6c42a5f2a02d87a43f1f008b1
markdflitter <markdflitter@googlemail.com>
2018-09-21 18:29:51 +0100

    more test tidying

---
+++ tests/6_0_blob_angle.t.cpp
@@ -0,0 +1,81 @@
+#include <gtest/gtest.h>
+#include <blob.h>
+
+TEST (test_6_0_blob_angle_t, north)
+{
+	Blob b1 = CreateBlob ().position (make_pt (5.0, 5.0));
+	Blob b2 = CreateBlob ().position (make_pt (5.0, 10.0));
+
+	double angle = b1.angle (b2);
+	EXPECT_DOUBLE_EQ (angle, 0.0);
+}
+
+TEST (test_6_0_blob_angle_t, south)
+{
+	Blob b1 = CreateBlob ().position (make_pt (-5.0, -5.0));
+	Blob b2 = CreateBlob ().position (make_pt (-5.0, -10.0));
+
+	double angle = b1.angle (b2);
+	EXPECT_DOUBLE_EQ (angle, M_PI);
+}
+
+TEST (test_6_0_blob_angle_t, east)
+{
+	Blob b1 = CreateBlob ().position (make_pt (5.0, 5.0));
+	Blob b2 = CreateBlob ().position (make_pt (10.0, 5.0));
+
+	double angle = b1.angle (b2);
+	EXPECT_DOUBLE_EQ (angle, M_PI / 2.0);
+}
+
+TEST (test_6_0_blob_angle_t, west)
+{
+	Blob b1 = CreateBlob ().position (make_pt (-5.0, 5.0));
+	Blob b2 = CreateBlob ().position (make_pt (-10.0, 5.0));
+
+	double angle = b1.angle (b2);
+	EXPECT_DOUBLE_EQ (angle, 3.0 * M_PI / 2.0);
+}
+
+TEST (test_6_0_blob_angle_t, north_east)
+{
+	Blob b1 = CreateBlob ().position (make_pt (5.0, 5.0));
+	Blob b2 = CreateBlob ().position (make_pt (10.0, 10.0));
+
+	double angle = b1.angle (b2);
+	EXPECT_DOUBLE_EQ (angle, M_PI / 4);
+}
+
+TEST (test_6_0_blob_angle_t, north_west)
+{
+	Blob b1 = CreateBlob ().position (make_pt (-5.0, 5.0));
+	Blob b2 = CreateBlob ().position (make_pt (-10.0, 10.0));
+
+	double angle = b1.angle (b2);
+	EXPECT_DOUBLE_EQ (angle, -M_PI / 4);
+}
+
+TEST (test_6_0_blob_angle_t, south_east)
+{
+	Blob b1 = CreateBlob ().position (make_pt (5.0, -5.0));
+	Blob b2 = CreateBlob ().position (make_pt (10.0, -10.0));
+
+	double angle = b1.angle (b2);
+	EXPECT_DOUBLE_EQ (angle, 3 * M_PI / 4);
+}
+
+TEST (test_6_0_blob_angle_t, south_west)
+{
+	Blob b1 = CreateBlob ().position (make_pt (-5.0, -5.0));
+	Blob b2 = CreateBlob ().position (make_pt (-10.0, -10.0));
+
+	double angle = b1.angle (b2);
+	EXPECT_DOUBLE_EQ (angle, 5 * M_PI / 4);
+}
+
+int main (int argc, char** argv) 
+{
+	testing::InitGoogleTest (&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
