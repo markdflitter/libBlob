16027a970419b92c0f9a185f40f461b83e40f798
markdflitter <markdflitter@googlemail.com>
2018-09-28 12:31:07 +0100

    more tests

--- tests/11_01_blob_chooseNextAction_findPossibleActions.t.cpp
+++ tests/11_01_blob_chooseNextAction_findPossibleActions.t.cpp
@@ -477,11 +477,59 @@ TEST (test_11_01_blob_chooseNextAction_findPossibleActions_t,
 	EXPECT_GT (actions_blob2[0].weight, actions_blob1[0].weight);
 }
 
+TEST (test_11_01_blob_chooseNextAction_findPossibleActions_t,
+  more_likely_to_attack_closer_blob_in_smell_range)
+{
+	std::vector<Blob> blobs {
+		CreateBlob ().HP (50U).damage (80U).aggression (0.5).position (make_pt (3.0, 4.0)),
+		CreateBlob ().HP (50U).damage (80U).smell (100.0).aggression (0.5).position (make_pt (5.0, 6.0)),
+		CreateBlob ().HP (50U).damage (1000U).smell (100.0).aggression (0.5).position (make_pt (10.0, 12.0))};
+	auto actions = blobs[2].findPossibleActions (blobs);
+
+	EXPECT_EQ (actions.size (), 4);
+	EXPECT_EQ (actions[0].action, Blob::ActionPossibility::attack);
+	EXPECT_EQ (actions[0].target, &blobs[0]);
+	EXPECT_EQ (actions[2].action, Blob::ActionPossibility::attack);
+	EXPECT_EQ (actions[2].target, &blobs[1]);
+	EXPECT_GT (actions[2].weight, actions[0].weight);
+}
+
+TEST (test_11_01_blob_chooseNextAction_findPossibleActions_t,
+  more_likely_to_attack_closer_blob_in_smell_range_even_if_there_is_a_weaker_one_further_away)
+{
+	std::vector<Blob> blobs {
+		CreateBlob ().HP (40U).damage (80U).aggression (0.5).position (make_pt (3.0, 4.0)),
+		CreateBlob ().HP (50U).damage (80U).smell (100.0).aggression (0.5).position (make_pt (5.0, 6.0)),
+		CreateBlob ().HP (50U).damage (1000U).smell (100.0).aggression (0.5).position (make_pt (10.0, 12.0))};
+	auto actions = blobs[2].findPossibleActions (blobs);
+
+	EXPECT_EQ (actions.size (), 4);
+	EXPECT_EQ (actions[0].action, Blob::ActionPossibility::attack);
+	EXPECT_EQ (actions[0].target, &blobs[0]);
+	EXPECT_EQ (actions[2].action, Blob::ActionPossibility::attack);
+	EXPECT_EQ (actions[2].target, &blobs[1]);
+	EXPECT_GT (actions[2].weight, actions[0].weight);
+}
+
+TEST (test_11_01_blob_chooseNextAction_findPossibleActions_t,
+  will_attack_distant_blob_in_smell_range_if_there_it_is_weak_enough)
+{
+	std::vector<Blob> blobs {
+		CreateBlob ().HP (1U).damage (80U).aggression (0.5).position (make_pt (3.0, 4.0)),
+		CreateBlob ().HP (50U).damage (80U).smell (100.0).aggression (0.5).position (make_pt (5.0, 6.0)),
+		CreateBlob ().HP (50U).damage (1000U).smell (100.0).aggression (0.5).position (make_pt (10.0, 12.0))};
+	auto actions = blobs[2].findPossibleActions (blobs);
+
+	EXPECT_EQ (actions.size (), 4);
+	EXPECT_EQ (actions[0].action, Blob::ActionPossibility::attack);
+	EXPECT_EQ (actions[0].target, &blobs[0]);
+	EXPECT_EQ (actions[2].action, Blob::ActionPossibility::attack);
+	EXPECT_EQ (actions[2].target, &blobs[1]);
+	EXPECT_GT (actions[0].weight, actions[2].weight);
+}
 
 // blobs in smell range
 // attack action
-    // more likely to attack closer, if the same
-    // more likely to attack closer, even if weaker avaiable further away
   // weaker blobs more likely to flee	
   //stronger blobs more likely to attack
   // more aggressive blobs more likely to attack
@@ -492,7 +540,6 @@ TEST (test_11_01_blob_chooseNextAction_findPossibleActions_t,
 // combined
 //  more likely to flee blobs in same square
 // may flee blobs further away
-
 //aggression randomness
 
 int main (int argc, char** argv) 
