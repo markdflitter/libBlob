fd1b889abda76ff6bf79986ef7d2eb7a60562a35
markdflitter <markdflitter@googlemail.com>
2018-10-10 12:27:25 +0100

    added tests for option

--- include/blob.h
+++ include/blob.h
@@ -14,36 +14,8 @@
 #include <movement.h>
 #include <fight.h>
 #include <rnd.h>
-#include <assert.h>
+#include "option.h"
 	
-enum action_t {attack, flee, wander};
-
-class Blob;
-
-class Option 
-{
-public:
-	Option (action_t action, double weight, Blob* target) :
-		_action (action)
-	      , _weight (weight)
-	      , _target (target)
-	{
-	} 
-	
-	action_t action () const {return _action;}
-	double weight () const {return _weight;}
-	Blob* target () const {return _target;}
-
-	bool matches (const Option& other)
-	{
-		return (_action == other._action) && (_target == other._target);
-	}
-private:
-	action_t _action;
-	double _weight;
-	Blob* _target;
-};
-
 class CreateBlob 
 {
 public:
